b5a8663b383b73cb68cee43a7da7cb0f1a7cfdc8
Berketunc <tnberkec@gmail.com>
2026-01-31 15:52:10 +0000

    Hyperparameter Tuning

--- train_model.py
+++ train_model.py
@@ -26,7 +26,14 @@ def train_model(symbol="AAPL"):
     #80-20 Train-Test Split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    model = RandomForestClassifier(n_estimators=200, min_samples_split=10, random_state=1)
+    model = RandomForestClassifier(
+        n_estimators = 500,
+        max_depth = 10,
+        min_samples_leaf = 5,
+        random_state = 1,
+        n_jobs = -1
+        )
+    
     model.fit(X_train, y_train)
 
     #Feature Importance
